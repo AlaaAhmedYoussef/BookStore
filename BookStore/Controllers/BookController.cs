e37f8ef18b20e058fcd65a2841cbb6d56bffb4bb
Alaa Ahmed Youssef <master_here100@yahoo.com>
2020-02-05 12:29:58 +0200

    migration from V2.1 to v3.1

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using BookStore.Models.Respositories;
+using BookStore.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public partial class BookController : Controller
+    {
+        private readonly IBookStoreRepository<Book> bookRepository;
+        private readonly IBookStoreRepository<Author> authorRepository;
+        private readonly IHostingEnvironment hosting;
+
+        public BookController(IBookStoreRepository<Book> _bookRepository,
+            IBookStoreRepository<Author> _authorRepository,
+            IHostingEnvironment _hosting)
+        {
+            this.bookRepository = _bookRepository;
+            this.authorRepository = _authorRepository;
+            hosting = _hosting;
+        }
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            var books = bookRepository.list();
+
+            return View(books);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            var book = bookRepository.Find(id);
+
+            return View(book);
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            var vModel = new BookAuthorViewModel
+            {
+                Authors = FillSelectList()
+            };
+            return View(vModel);
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BookAuthorViewModel vModel)
+        {
+            // if all validations are true(achieved)
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string fileName = UploadFile(vModel.File) ?? string.Empty;
+
+                    if (vModel.AuthorId < 0)
+                    {
+                        ViewBag.Message = "Please select an Author from the list";
+
+                        return View(GetAllAuthors());
+                    }
+                    var author = authorRepository.Find(vModel.AuthorId);
+                    var book = new Book
+                    {
+                        Id = vModel.BookId,
+                        Title = vModel.Title,
+                        Description = vModel.Description,
+                        ImageUrl = fileName,
+                        Author = author
+                    };
+                    bookRepository.Add(book);
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            //ModelState will passed to validation-summary
+            ModelState.AddModelError("", "You have to fill all the required fields");
+            return View(GetAllAuthors());
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var book = bookRepository.Find(id);
+            var authorId = book.Author == null ? book.Author.Id = 0 : book.Author.Id;
+            var vModel = new BookAuthorViewModel
+            {
+                BookId = book.Id,
+                Title = book.Title,
+                Description = book.Description,
+                ImageUrl = book.ImageUrl,
+                AuthorId = authorId,
+                Authors = authorRepository.list().ToList()
+            };
+
+            return View(vModel);
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, BookAuthorViewModel vModel)
+        {
+            try
+            {
+                string fileName = UploadFile(vModel.File, vModel.ImageUrl);
+
+                var author = authorRepository.Find(vModel.AuthorId);
+                var book = new Book
+                {
+                    Id = id,
+                    Title = vModel.Title,
+                    Description = vModel.Description,
+                    ImageUrl = fileName,
+                    Author = author
+                };
+                bookRepository.Update(id, book);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var book = bookRepository.Find(id);
+            
+            return View(book);
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmDelete(int id)
+        {
+            try
+            {
+                bookRepository.Delete(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Search(string term)
+        {
+            var result = bookRepository.Search(term);
+
+            return View("Index", result);
+        }
+    }
+}
---
+++ BookStore/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using BookStore.Models.Database;
+using BookStore.Models.Respositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookStore
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddScoped<IBookStoreRepository<Author>, AuthorDBRepository>();
+            services.AddScoped<IBookStoreRepository<Book>, BookDBRepository>();
+            services.AddDbContext<BookStoreDbContext>( options => 
+            {
+                options.UseSqlServer(configuration.GetConnectionString("SqlCon"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvc(route =>
+            {
+                route.MapRoute("default", "{controller=Book}/{action=Index}/{id?}");
+            });
+            /*
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+            */
+        }
+    }
+}
